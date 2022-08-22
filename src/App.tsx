c1670f412a9ea4c0034709a860a525d4954b4aae
NguyenNgocBaoChung <chung.nnb@alta.com.vn>
2022-08-22 13:56:29 +0700

    update code

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import logo from "./logo.svg";
-import "./App.css";
+import "./App.scss";
 import { Camera } from "@mediapipe/camera_utils";
 import { Objectron } from "@mediapipe/objectron";
 import drawingUtils from "@mediapipe/drawing_utils";
@@ -36,10 +36,6 @@ function App() {
             (x: { point2d: { x: number; y: number; depth: number } }) =>
               x.point2d,
           );
-          console.debug("~detectedObject", detectedObject);
-          console.debug("~landmarks", landmarks);
-          console.debug("~landmarks[0]", landmarks[0]);
-
           // Draw bounding box.
           canvasCtx &&
             drawingUtils.drawConnectors(
@@ -85,33 +81,6 @@ function App() {
             drawingUtils.drawLandmarks(canvasCtx, [landmarks[8]], {
               color: "white",
             });
-          // const gradient = canvasCtx!.createLinearGradient(100, 100, 300, 300);
-          // gradient.addColorStop(0, "red");
-          // gradient.addColorStop(0.5, "green");
-          // gradient.addColorStop(1, "blue");
-
-          // canvasCtx!.fillStyle = gradient; // fill color
-          // canvasCtx!.strokeStyle = gradient; // stroke color
-          // canvasCtx!.lineWidth = 20; // stroke width
-
-          // // diagonal rectanges
-          // canvasCtx!.fillRect(0, 0, 100, 100);
-          // canvasCtx!.fillRect(50, 50, 100, 100);
-          // canvasCtx!.fillRect(100, 100, 100, 100);
-          // canvasCtx!.fillRect(150, 150, 100, 100);
-          // canvasCtx!.fillRect(200, 200, 100, 100);
-          // canvasCtx!.fillRect(250, 250, 100, 100);
-          // canvasCtx!.fillRect(300, 300, 100, 100);
-          // canvasCtx!.fillRect(350, 350, 100, 100);
-          // canvasCtx!.fillRect(400, 400, 100, 100);
-
-          // // side rectangles
-          // canvasCtx!.strokeRect(300, 100, 100, 100);
-          // canvasCtx!.fillRect(100, 300, 100, 100);
-
-          // // right corner rectangles
-          // canvasCtx!.fillRect(650, 50, 100, 100);
-          // canvasCtx!.strokeRect(650, 350, 100, 100);
         }
       }
       canvasCtx!.restore();
@@ -134,16 +103,22 @@ function App() {
       onFrame: async () => {
         await objectron.send({ image: videoElement });
       },
-      width: 1920,
-      height: 1200,
+      // width: 1920,
+      // height: 1200,
     });
     camera.start();
   }, []);
 
   return (
     <div className="container">
-      <video className="input_video" style={{ display: "none" }} />
-      <canvas className="output_canvas" width="1920px" height="1200px" />
+      <video className="input_video" />
+      <div className="canvas-container">
+        <canvas
+          className="output_canvas"
+          width="1920px"
+          height="1200px"
+        ></canvas>
+      </div>
     </div>
   );
 }
